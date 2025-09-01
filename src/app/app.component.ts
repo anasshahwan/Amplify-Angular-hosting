d0880168c6caa66696cfdfdabbbc33f9bef46f5a
Anas Shahwan <anas.shahwan20@gmail.com>
2025-09-01 17:02:14 -0400

    title edited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'amplify-angular-hosting';
+  title = 'Github-Angular-amplify';
 }
